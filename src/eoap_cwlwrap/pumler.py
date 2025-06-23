e1c8fc03c09a53fa984751468782d18d3a59b028
stripodi <simone.tripodi@terradue.com>
2025-06-23 16:16:16 +0200

    feat: better PlantUML layout and components declaration

--- src/eoap_cwlwrap/pumler.py
+++ src/eoap_cwlwrap/pumler.py
@@ -28,39 +28,39 @@ If not, see <https://creativecommons.org/licenses/by-sa/4.0/>.
 @startuml
 
 {% for workflow in workflows %}
-component "{{ workflow.id }}" as {{ to_puml_name(workflow.id) }} {
-    {% for input in workflow.inputs %}portin "{{ input.id }}" as {{ to_puml_name(workflow.id) }}_{{ to_puml_name(input.id) }}
+package "Workflow '{{ workflow.id }}'" {
+    component "{{ workflow.id }}" as {{ to_puml_name(workflow.id) }} {
+    {% for input in workflow.inputs %}
+        portin "{{ input.id }}" as {{ to_puml_name(workflow.id) }}_{{ to_puml_name(input.id) }}
     {% endfor %}
-    {% for output in workflow.outputs %}portout "{{ output.id }}" as {{ to_puml_name(workflow.id) }}_{{ to_puml_name(output.id) }}
+    {% for output in workflow.outputs %}
+        portout "{{ output.id }}" as {{ to_puml_name(workflow.id) }}_{{ to_puml_name(output.id) }}
+    {% endfor %}
+    }
+
+{% for step in workflow.steps %}
+    component "{{ step.id }}" as {{ to_puml_name(workflow.id) }}_{{ to_puml_name(step.id) }} {
+    {% for input in step.in_ %}
+        portin "{{ input.id }}" as {{ to_puml_name(workflow.id) }}_{{ to_puml_name(step.id) }}_{{ to_puml_name(input.id) }}
+        {{ to_puml_name(workflow.id) }}_{{ input.source | replace('/', '_') | replace('-', '_') }} -down-> {{ to_puml_name(workflow.id) }}_{{ to_puml_name(step.id) }}_{{ to_puml_name(input.id) }}
     {% endfor %}
 
-    {% if workflow.steps is defined %}
-    {% for step in workflow.steps %}component "{{ step.id }}" as {{ to_puml_name(workflow.id) }}_{{ to_puml_name(step.id) }} {
-        {% for input in step.in_ %}portin "{{ input.id }}" as {{ to_puml_name(workflow.id) }}_{{ to_puml_name(step.id) }}_{{ to_puml_name(input.id) }}
-        {% endfor %}
-        {% for output in step.out %}portout "{{ output }}" as {{ to_puml_name(workflow.id) }}_{{ to_puml_name(step.id) }}_{{ to_puml_name(output) }}
-        {% endfor %}
-    }
+    {% for output in step.out %}portout "{{ output }}" as {{ to_puml_name(workflow.id) }}_{{ to_puml_name(step.id) }}_{{ to_puml_name(output) }}
     {% endfor %}
-    {% endif %}
+    }
+{% endfor %}
 }
 {% endfor %}
 
 {% for workflow in workflows %}
-    {% if workflow.steps is defined %}
-        {% for step in workflow.steps %}
-            {% for input in step.in_ %}
-{{ to_puml_name(workflow.id) }}_{{ input.source | replace('/', '_') | replace('-', '_') }} --> {{ to_puml_name(workflow.id) }}_{{ to_puml_name(step.id) }}_{{ to_puml_name(input.id) }}
-            {% endfor %}
-
-{{ to_puml_name(workflow.id) }}_{{ to_puml_name(step.id) }} -> {{ step.run[1:] | replace('-', '_') }}
-        {% endfor %}
-    {% endif %}
-
     {% for output in workflow.outputs %}
         {% for outputSource in output.outputSource %}
-{{ to_puml_name(workflow.id) }}_{{ outputSource | replace('/', '_') | replace('-', '_') }} --> {{ to_puml_name(workflow.id) }}_{{ to_puml_name(output.id) }}
+            {{ to_puml_name(workflow.id) }}_{{ outputSource | replace('/', '_') | replace('-', '_') }} -up-> {{ to_puml_name(workflow.id) }}_{{ to_puml_name(output.id) }}
         {% endfor %}
+    {% endfor %}
+
+    {% for step in workflow.steps %}
+{{ to_puml_name(workflow.id) }}_{{ to_puml_name(step.id) }} -right-> {{ step.run[1:] | replace('-', '_') }}
     {% endfor %}
 {% endfor %}
 @enduml
