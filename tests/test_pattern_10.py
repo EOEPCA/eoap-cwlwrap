5136392120d12afda9a4a7c3bce99be82a24a76b
Fabrice Brito <fabrice.brito@terradue.com>
2025-06-26 17:42:15 +0200

    adds tests for pattern 10, minor fix

---
+++ tests/test_pattern_10.py
@@ -0,0 +1,21 @@
+import os
+from tests.helpers import TestCWL
+
+class TestPattern10(TestCWL):
+
+    def setUp(self):
+        super().setUp()
+        self.app_cwl_file = os.path.join(os.path.dirname(__file__), "pattern-10/workflow.cwl")
+        self.entrypoint = "pattern-10"
+
+    def tearDown(self):
+        super().tearDown()
+
+    def test_pattern_validation(self):
+        self._cwl_validation(self.app_cwl_file)
+
+    def test_pattern_wrapped_cwl(self):
+        self._wrapped_cwl_validation()
+        
+
+    
