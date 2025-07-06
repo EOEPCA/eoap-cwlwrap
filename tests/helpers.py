eb6b9e19305e19b0b9396f98a558b21d224f6424
stripodi <simone.tripodi@terradue.com>
2025-07-06 09:58:17 +0200

    feat: added missing license header

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,3 +1,13 @@
+"""
+EOAP CWLWrap (c) 2025
+
+EOAP CWLWrap is licensed under
+Creative Commons Attribution-ShareAlike 4.0 International.
+
+You should have received a copy of the license along with this work.
+If not, see <https://creativecommons.org/licenses/by-sa/4.0/>.
+"""
+
 import os
 import unittest
 from cwltool.main import main as cwlmain
